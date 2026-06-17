ca936808363739751ccaed1411e5c7efe2075e42
Dhaval Mittal <dhavalmittal13@gmail.com>
2026-06-17 12:07:45 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
         System.out.print("$ ");
+        String a=sc.next();
+        System.err.print(a+": command not found");
     }
 }
